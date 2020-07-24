1d94e6df0b674c383349eeb67ca73e97f4ec1d5b
PeatMan <lucas.petka@gmail.com>
2020-07-24 15:30:48 +0300

    Created products and weather conditions tables with seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,18 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        DB::table('weather_conditions')->insert(['name' => 'clear']);
+        DB::table('weather_conditions')->insert(['name' => 'isolated-clouds']);
+        DB::table('weather_conditions')->insert(['name' => 'scattered-clouds']);
+        DB::table('weather_conditions')->insert(['name' => 'overcast']);
+        DB::table('weather_conditions')->insert(['name' => 'light-rain']);
+        DB::table('weather_conditions')->insert(['name' => 'moderate-rain']);
+        DB::table('weather_conditions')->insert(['name' => 'heavy-rain']);
+        DB::table('weather_conditions')->insert(['name' => 'sleet']);
+        DB::table('weather_conditions')->insert(['name' => 'light-snow']);
+        DB::table('weather_conditions')->insert(['name' => 'moderate-snow']);
+        DB::table('weather_conditions')->insert(['name' => 'heavy-snow']);
+        DB::table('weather_conditions')->insert(['name' => 'fog']);
+        DB::table('weather_conditions')->insert(['name' => 'na']);
     }
 }
